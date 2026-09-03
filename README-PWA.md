157a69a975464bfb5df74415671e7a611d734312
tamirzeharya-dot <tamir.zeharya@gmail.com>
2026-09-03 13:18:05 +0300

    Add files via upload

---
+++ README-PWA.md
@@ -0,0 +1,4 @@
+# Tamir Dive Log PWA Launcher
+
+קבצי מעטפת PWA עבור "יומן הצלילה של תמיר".
+ה־launcher מותקן עם אייקון מותאם ומעביר לאפליקציית Google Apps Script הקיימת.
---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE='tamir-dive-launcher-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if (new URL(e.request.url).origin === self.location.origin) {
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
