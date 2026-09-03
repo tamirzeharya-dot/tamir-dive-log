b67201e70edffff91494061cd31e7503df07793e
tamirzeharya-dot <tamir.zeharya@gmail.com>
2026-09-03 14:27:32 +0300

    Refresh PWA service worker cache

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-const CACHE='tamir-dive-launcher-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE='tamir-dive-launcher-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png?v=2','./icon-512.png?v=2'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',e=>{
   if (new URL(e.request.url).origin === self.location.origin) {
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
   }
 });
